24b2c5629de50b1d80fb43e45eed6820ddc1cb61
Luiz Filipe Santos <150827818+devLoue@users.noreply.github.com>
2025-04-27 16:15:42 -0300

    Aulas rad quarta

---
+++ Desenvolvimento rápido com PYTHON/Aulas/Aula 2/funcao.py
@@ -0,0 +1,5 @@
+
+def soma (n1, n2): ##Usamos construtor def para definir o nome da função, soma foi um nome escolhido por mim
+    return n1+n2 #A funcao soma retorna sempre um número mais outro número passado por parametro, ou seja, soma sempre retorna n1+n2 (numero1 + numero2)
+
+print(soma(2,5)) #chamando função soma dentro de um print pra já ser exibido o retorno...
---
+++ Desenvolvimento rápido com PYTHON/Aulas/Aula 2/type.py
@@ -0,0 +1,10 @@
+## Soma simples de dois números inteiros
+soma = 2 + 2
+print(soma)
+print(type(soma)) 
+## type mostra o tipo da variável soma, que neste caso, é um inteiro. 
+
+##Concatenando
+soma = "2" + "2"
+print(soma) ##Neste caso, a soma também é "22", porém, foi feita uma concatenação de string. A  variável soma recebe a string "2"+ a string "2", mostrando como 22. 
+print(type(soma)) ## Como foi dito na linha de cima, soma agora é uma string, e a função type nos mostra isso. 
