a1dd975fa1254b675ee3180f324f7242e847ba5b
Luiz Filipe Santos <150827818+devLoue@users.noreply.github.com>
2025-04-28 20:24:23 -0300

    Função open pt1

--- Desenvolvimento rápido com PYTHON/Aulas/Aula 4/open.py
+++ Desenvolvimento rápido com PYTHON/Aulas/Aula 4/open.py
@@ -1,10 +1,10 @@
-arquivo = open("meu_arquivo.txt")
-print (arquivo, "Arquivo aberto")
-arquivo.close
-print("Fechando arquivo...") 
+arquivo = open("arquivo.txt", "r") #Abrindo o arquivo "meu_arquivo.txt" e o atribuindo á uma variavel chamada arquivo
+print (arquivo)
+print(f"Nome do arquivo: {arquivo.name}") #Exibindo nome do arquivo
+print(f"Modo do arquivo: {arquivo.mode}") #Exibindo o modo, se é leitura, se é escrita...
+print(f'O arquivo foi fechado? {arquivo.closed}') #a função .closed é um booleano que retornará false caso o arquivo esteja aberto e true caso esteja fechado
+print(f"Exibindo o conteúdo do arquivo txt: {arquivo.read()}")
+print("Fechando arquivo...")
+arquivo.close()
+print(f'O arquivo foi fechado? {arquivo.closed}') #Retornando true, já que fechamos na linha acima
 
-try:
-    with open('meu_arquivo.txt', 'w') as file:
-        file.write('Este é o novo conteúdo.')
-except FileNotFoundError:
-    print("Erro ao abrir o arquivo.")
