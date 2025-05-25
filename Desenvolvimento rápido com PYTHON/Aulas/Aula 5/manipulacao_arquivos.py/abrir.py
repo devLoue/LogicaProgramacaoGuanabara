120136872474277c9b3a56f709612e0ff428829d
Luiz Filipe Santos <150827818+devLoue@users.noreply.github.com>
2025-05-25 18:51:09 -0300

    Manipulacao de arquivos, write

--- Desenvolvimento rápido com PYTHON/Aulas/Aula 5/manipulacao_arquivos.py/abrir.py
+++ Desenvolvimento rápido com PYTHON/Aulas/Aula 5/manipulacao_arquivos.py/abrir.py
@@ -2,11 +2,18 @@
 #open(“../arquivo.txt”), para os casos em que o arquivo está no diretório acima do script.
 
 arquivo = open("arquivo.txt") ##método padrão r(read), modo de leitura.
-for palavra in arquivo: #para cada palavra dentro do arquivo 
-    print(palavra)
+conteudo = arquivo.readlines()
+print("type arquivo:",type(arquivo))
+print("type conteudo: ",type(conteudo)) ##conteudo = arquivo.readlines() é uma lista
+print("arquivo.nome: ",arquivo.name)
+print("arquivo.mode: ",arquivo.mode)
+print("arquivo.closed: ",arquivo.closed)#False antes do arquivo.close()
+print("arquivo.closed: ",arquivo.closed)##True após o arquivo.close()
+
+for linha in arquivo:
+    print(linha)
 
 
-
-
-    
-arquivo.close
+"""print(arquivo.read()) #Retorna todo o conteúdo de um arquivo como uma única string.
+print(arquivo.readline()) #Retorna uma linha de arquivo, incluindo caracteres de final (\n ou \r\n), e avança o cursor para a próxima.
+print(arquivo.readlines()) #Retorna uma lista em que cada item da lista é uma linha do arquivo"""
---
+++ Desenvolvimento rápido com PYTHON/Aulas/Aula 5/manipulacao_arquivos.py/escrever.py
@@ -0,0 +1,7 @@
+arquivo = open("arquivo2.txt","w")
+arquivo.write("Conteudo da primeira linha ")
+arquivo.write("Conteudo da segunda linha ")
+arquivo.write("Teste numero 3 ")
+
+arquivo.close()
+modoleitura = open("arquivo2.txt")
